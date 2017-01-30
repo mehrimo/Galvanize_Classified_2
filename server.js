6dd60709f0b20aded849bc0df3dd882efc8487ce
Mehri Russo <mehrimova@gmail.com>
2017-01-30 14:42:02 -0700

    installed dependincies

--- server.js
+++ server.js
@@ -13,6 +13,12 @@ app.use(bodyParser.json());
 app.use(express.static('./public'));
 app.use('/classifieds',messages);
 
+
+app.use('/jquery', express.static('node_modules/jquery/dist'));
+app.use('/bootstrap', express.static('node_modules/bootstrap/dist/js'));
+app.use('/angular', express.static('node_modules/angular'));
+app.use('/angular-ui-router', express.static('node_modules/angular-ui-router/release'));
+
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
